c0ff8770e7dd110e35dda06556463b5d84c15cab
Klenzdo <fagwaigbo@gmail.com>
2024-09-22 20:48:35 +0100

    God bless you

---
+++ src/main/java/com/saturday/free/FreeMan.java
@@ -0,0 +1,10 @@
+package com.saturday.free;
+
+public class FreeMan {
+    public static void main(String[] args) {
+        int a =10;
+        int b =5;
+         int totalSum = (a+b);
+        System.out.println(totalSum);
+    }
+}
